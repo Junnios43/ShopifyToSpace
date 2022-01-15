6d50118bae1beed78b8e00b6836019ceaf7dfe87
Jun <junjiewang42@outlook.com>
2022-01-15 12:40:57 -0500

    Added material UI card component and heart package

---
+++ shopifytospace/components/ImageCard.js
@@ -0,0 +1,35 @@
+import Heart from "react-animated-heart";
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const ImageCard = ({ title, description, date }) => {
+  const [isClick, setClick] = useState(false);
+  return (
+    <Card sx={{ maxWidth: 345, mt: 5 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default ImageCard;
